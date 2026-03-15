6879ff0c3688e3fc7e2cc3cdac8a23c4dd7c417c
Juan David Torres Torres <juan.torrestorres@unicolombo.edu.co>
2026-03-14 23:13:49 -0500

    correccion

--- src/controllers/notas.controller.js
+++ src/controllers/notas.controller.js
@@ -4,6 +4,7 @@ const obtenerNotas = async (req, res, next) => {
   try {
     const { usuario, password } = req.user;
     const notas = await obtenerNotasDesdePortal(usuario, password);
+    console.log('[NOTAS]', JSON.stringify(notas, null, 2));
     res.json({ notas });
   } catch (error) {
     next(error);
